8324017173c5034896963bdf7c3d2886fefacda3
gnavyas <navyagodavarti@gmail.com>
2020-05-31 22:57:28 +0530

    uu

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ class TestCalculator(unittest.TestCase):#unittest module provides a set of tools
 		self.assertEqual(b"None", solution.data)
 
     
-    def test_sub1(self):
+	def test_sub1(self):
 
 		#case 1, A is n integer B is an integer
 		solution = self.app.get('/sub?A=10&B=2')
@@ -145,9 +145,9 @@ class TestCalculator(unittest.TestCase):#unittest module provides a set of tools
 		solution = self.app.get('sub?A=57&B=400/0')
 		self.assertEqual(b"None", solution.data)
     
-    def test_mul1(self):   
+	def test_mul1(self):   
 
-       	#case 1, A is n integer B is an integer
+		#case 1, A is n integer B is an integer
 		solution = self.app.get('/mul?A=40&B=6')
 		self.assertEqual(b'240.0', solution.data)
     
